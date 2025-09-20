137d694837f7d65115bbe3e90463224e6725fe32
dma72-ua <dma72@alu.ua.es>
2025-09-20 20:53:26 +0200

    Añadidos los tubos y el funcionamiento de que aparezcan, se muevan y desaparezcan

--- src/MainGameState.cpp
+++ src/MainGameState.cpp
@@ -23,7 +23,30 @@ void MainGameState::update(float deltaTime)
 {
     player.vy += gravedad * deltaTime;
     player.y += player.vy * deltaTime;
-    // player.vy = 0;
+
+    spawnTimer += deltaTime;
+    if(spawnTimer >= spawnEvery) {
+        spawnTimer = 0.0f;
+
+        int pipe_y_offset_top = GetRandomValue(PIPE_H/2, GetScreenHeight()/2);
+        int x = GetScreenWidth();
+
+        int y_top = -pipe_y_offset_top;
+        int y_bot = (PIPE_H - pipe_y_offset_top) + GetRandomValue(PIPE_H/2, GetScreenHeight()/2);
+
+        Rectangle top{x, y_top, PIPE_W, PIPE_H}, bot{x, y_bot, PIPE_W, PIPE_H};
+        PipePair pipe_pair{top, bot};
+        
+        pipes.emplace_back(pipe_pair);
+    }
+
+    for(auto& pipe : pipes) {
+        pipe.top.x -= PIPE_SPEED * deltaTime;
+        pipe.bot.x -= PIPE_SPEED * deltaTime;
+    }
+
+    if(!pipes.empty() && pipes.front().bot.x < 0)
+        pipes.pop_front();
 }
 
 void MainGameState::render()
@@ -35,6 +58,11 @@ void MainGameState::render()
             DrawText("Bienvenido a Flappy Bird DCA", 20, 256, 15, BLACK);
 
             DrawCircle(player.x, player.y, 17.0, RED);
+
+            for(auto& pipe : pipes) {
+                DrawRectangleRec(pipe.top, GREEN);
+                DrawRectangleRec(pipe.bot, GREEN);
+            }
 
         EndDrawing();
 }
--- src/MainGameState.hpp
+++ src/MainGameState.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include "GameState.hpp"
+#include <deque>
+
 extern "C" {
     #include "raylib.h"
 }
@@ -8,6 +10,12 @@ struct Bird
 {
   int x, y;
   float vy;
+};
+
+struct PipePair
+{
+  Rectangle top, bot;
+  bool scored = false;
 };
 
 class MainGameState : public GameState
@@ -29,4 +37,15 @@ class MainGameState : public GameState
         char entered_key;
         Bird player;
         int gravedad;
+
+        std::deque<PipePair> pipes;
+
+        static constexpr int PIPE_W = 32;
+        static constexpr int PIPE_H = 320;
+
+        static constexpr int PIPE_GAP = 120;
+        static constexpr float PIPE_SPEED = 120.0f;
+
+        float spawnTimer = 0.0f;
+        float spawnEvery = 1.6f;
 };
